8c6a036bcb16cee5580ca25b39d0a35caf734505
“e <chuyangcy@163.com>
2020-09-10 23:14:42 +0800

    ...

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">高贤君11111111111111</router-link> |
+      <router-link to="/about">222222222222222222222222222</router-link>
     </div>
     <router-view/>
   </div>
